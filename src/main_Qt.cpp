ccf8c5f9f3e48bc633abf7314976c6de87d1c173
Christian M <christian.masser@gmail.com>
2011-10-16 22:54:03 +0200

    Began to change architecture for migration to Qt

---
+++ src/main_Qt.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 02.10.2011
+ *      Author: brainstar
+ */
+
+#include "Game.h"
+#include <iostream>
+#include <SDL/SDL.h>
+#include <SDL/SDL_video.h>
+using namespace std;
+
+int main(int arg, char *argv[])
+{
+	return 1;
+}
